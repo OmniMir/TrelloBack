07f8d02414130ea283624fe44ca63daa49469b7e
kapsilon <kapsilon47@gmail.com>
2018-09-21 13:39:52 +0300

    Orgs->Boards->Lists->Cards
    Get that cars in some order

--- main.go
+++ main.go
@@ -33,18 +33,20 @@ type Lists struct {
 type Cards struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
+type Card struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"desc"`
+	Labels       string `json:"idLabels"`
+	URL          string `json:"shortUrl"`
+	LastActivity string `json:"dateLastActivity"`
 }
 
 type Card struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
-	Description string `json:"desc"`
-	Labels string `json:"idLabels"`
-	URL string `json:"shortUrl"`
-	LastActivity string `json:"dateLastActivity"`
-
+	URL string `json:"name"`
 }
-
 
 func main() {
 	//Reading configuration file and getting authorization keys
@@ -61,38 +63,40 @@ func main() {
 	fmt.Println(myOrgs)
 	fmt.Println("")
 
-
-
 	//Getting all boards of organization
-	originBoards := getResponse("organizations/" + myOrgs[0].ID + "/boards", myAuth)
 	var myBoards []Boards
+	originBoards := getResponse("organizations/"+myOrgs[0].ID+"/boards", myAuth)
 	json.Unmarshal(originBoards, &myBoards)
 	fmt.Println(myBoards)
 	fmt.Println("")
 
 	//Getting all lists of board
-	originLists := getResponse("boards/" + myBoards[1].ID + "/lists", myAuth)
 	var mylists []Lists
+	originLists := getResponse("boards/"+myBoards[1].ID+"/lists", myAuth)
 	json.Unmarshal(originLists, &mylists)
 	fmt.Println(mylists)
 	fmt.Println("")
 
 	//Getting all cards of list
-	originCards := getResponse("list/" + mylists[2].ID +"/cards", myAuth)
 	var myCards []Cards
+	originCards := getResponse("list/"+mylists[2].ID+"/cards", myAuth)
 	json.Unmarshal(originCards, &myCards)
 	//fmt.Println(myCards)
 	fmt.Println(myCards)
 	fmt.Println("")
 
 	//Getting all of card
-
-	originCard := getResponse("cards/" + myCards[3].ID, myAuth)
 	var myCard Card
 	json.Unmarshal(originCard, &myCard)
 	fmt.Println(myCard)
 	fmt.Println("")
 
+	//Getting card attachments
+	originAttachments := getResponse("cards/"+myCards[3].ID+"/attachments", myAuth)
+	var myAttachments []Attachments
+	json.Unmarshal(originAttachments, &myAttachments)
+	fmt.Println(myAttachments)
+	fmt.Println("")
 }
 
 func getResponse(request string, authorization string) []byte {
